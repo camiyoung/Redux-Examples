b7305fc8e915953e4690efd8898bc575afe2435e
camiyoung <anji6978@naver.com>
2022-06-13 23:32:52 +0900

    알림 기능 추가

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ import { AddPostForm } from './features/posts/AddPostForm'
 import EditPostForm from './features/posts/EditPostForm'
 import PostList from './features/posts/PostList'
 import SinglePostPage from './features/posts/SinglePostPage'
-
+import UserList from './features/users/UserList'
+import UserPage from './features/users/UserPage'
+import NotificationsList from './features/notifications/NotificationsList'
 function App() {
   return (
     <Router>
@@ -31,7 +33,9 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
-
+          <Route exact path="/users" component={UserList} />
+          <Route exact path="/users/:userId" component={UserPage} />
+          <Route exact path="/notification" component={NotificationsList} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import postsReducer from '../features/posts/postsSlice'
 import userReducer from '../features/users/userSlice'
+import notificationsReducer from '../features/notifications/notificationsSlice'
 
 export default configureStore({
   reducer: {
     posts: postsReducer,
     users: userReducer,
+    notifications: notificationsReducer,
   },
 })
